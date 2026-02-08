13b0270122244807eb5a681446ead1186ee982a1
vsubbaram <vigneshsubbaram@gmail.com>
2026-02-08 21:39:50 +0000

    feat(docker): add Dockerfile and GitHub Actions workflow for building and pushing mobilefarm image

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.13-slim-bookworm
+
+RUN apt-get update -y && apt install -y git gcc unzip wget && pip install uv && \
+    git clone https://github.com/vigneshsubbaram/pytest-mobilefarm.git && \
+    cd pytest-mobilefarm && uv pip install . --system && \
+    git clone https://github.com/vigneshsubbaram/mobilefarm.git && \
+    cd mobilefarm && uv pip install . --system && \
+    wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip \
+    -O /tmp/platform-tools-latest-linux.zip \
+    && unzip /opt/platform-tools-latest-linux.zip -d /opt/ && \
+    rm /opt/platform-tools-latest-linux.zip
+
+ENV ANDROID_HOME=/opt/platform-tools
+ENV PATH="${ANDROID_HOME}:${PATH}"
+
+WORKDIR /workspace
+
+CMD ["/bin/bash"]
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,6 +5,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.webdriver import WebDriver
 from pytest_boardfarm3.lib.test_logger import TestLogger
 
+
 @pytest.mark.env_req({"environment_def": {"pixel8_pro": {}}})
 def test_get_current_software_version(
     bf_logger: TestLogger,
@@ -18,16 +19,16 @@ def test_get_current_software_version(
     bf_logger.log_step("Open About phone")
     about_phone = driver.find_element(
         AppiumBy.ANDROID_UIAUTOMATOR,
-        'new UiScrollable(new UiSelector().scrollable(true))'
-        '.scrollIntoView(new UiSelector().text("About phone"))'
+        "new UiScrollable(new UiSelector().scrollable(true))"
+        '.scrollIntoView(new UiSelector().text("About phone"))',
     )
     about_phone.click()
 
     bf_logger.log_step("Scroll to Build number")
     build_number_el = driver.find_element(
         AppiumBy.ANDROID_UIAUTOMATOR,
-        'new UiScrollable(new UiSelector().scrollable(true))'
-        '.scrollIntoView(new UiSelector().text("Build number"))'
+        "new UiScrollable(new UiSelector().scrollable(true))"
+        '.scrollIntoView(new UiSelector().text("Build number"))',
     )
 
     build_number = build_number_el.text
